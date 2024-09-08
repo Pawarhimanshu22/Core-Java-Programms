66cdbb4b2580349f656a1877ae172bdc51be9116
Pawarhimanshu22 <himanshupawarofficial2203@gmail.com>
2024-09-08 22:01:33 +0530

    Understanding of data types in java

---
+++ Data Types/RealNumberOfData.java
@@ -0,0 +1,12 @@
+public class RealNumberOfData {
+    public static void main(String[] args) {
+        // float data = 4.5;
+        float data = 4.5f; // f is mendetory
+        System.out.println(data);
+
+    double num1 = 38948.444;
+    double num2 = 4444.555;
+double res = num1 + num2;
+System.out.println(res);
+    }
+}
